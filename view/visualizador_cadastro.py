2ee251ef34215ab9d33106a475ba592e93f5570f
Gabriel Sartori Rangel <gabriel.sartorirangel@gmail.com>
2022-12-08 14:42:08 -0300

    pastas

--- view/visualizador_cadastro.py
+++ view/visualizador_cadastro.py
@@ -1,6 +1,6 @@
 import tkinter as tk
-from visualizador import Visualizador
-from controlador_cadastro import ControladorCadastro
+from view.visualizador import Visualizador
+from control.controlador_cadastro import ControladorCadastro
 import random
 
 
